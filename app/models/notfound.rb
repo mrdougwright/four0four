f4615ea5d722a9a871545ea7c6122c36ed74df5f
Doug Wright <mrdougwright@gmail.com>
2014-02-19 15:07:59 -0800

    imgkit paperclip upload_image method in model

--- app/models/notfound.rb
+++ app/models/notfound.rb
@@ -1,16 +1,19 @@
 class Notfound < ActiveRecord::Base
   has_attached_file :site_404, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
 
-  before_save :grab_screenshot
+  before_save :upload_image
 
-  def grab_screenshot
-    img = screenshot
-  end
+  def upload_image
+    model = self
+    html = "http://#{site_url}/foodougbar"
+    kit = IMGKit.new(html)
+    img = kit.to_img(:png)
+    file = Tempfile.new(["template_#{model.id}", "png"], "tmp", encoding: 'ascii-8bit')
 
-  private
-
-  def screenshot
-    kit = IMGKit.new("http://#{site_url}/foodougbar").to_png
-    kit.to_file(Rails.root + "tmp/testimage.png")
+    file.write(img)
+    file.flush
+    model.site_404 = file
+    model.save
+    file.unlink
   end
 end
