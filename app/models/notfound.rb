cc736c406e834e812513cf24509f4996a63904f7
Doug Wright <mrdougwright@gmail.com>
2014-02-19 11:35:32 -0800

    paperclip

--- app/models/notfound.rb
+++ app/models/notfound.rb
@@ -1,3 +1,12 @@
+require 'rubygems'
+require 'selenium-webdriver'
+
 class Notfound < ActiveRecord::Base
   has_attached_file :site_404, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+
+  before_save :grab_screenshot
+
+  def grab_screenshot
+
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140219185810) do
+ActiveRecord::Schema.define(version: 20140219191016) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,10 @@ ActiveRecord::Schema.define(version: 20140219185810) do
     t.string   "site_url"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "site_404_file_name"
+    t.string   "site_404_content_type"
+    t.integer  "site_404_file_size"
+    t.datetime "site_404_updated_at"
   end
 
 end
