8425138429bd4d31abd32a8157c9c81177271181
Doug Wright <mrdougwright@gmail.com>
2014-02-19 13:27:55 -0800

    img magick added

--- app/models/notfound.rb
+++ app/models/notfound.rb
@@ -1,12 +1,12 @@
-require 'rubygems'
-require 'selenium-webdriver'
-
 class Notfound < ActiveRecord::Base
   has_attached_file :site_404, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
 
   before_save :grab_screenshot
 
   def grab_screenshot
-
+    puts site_url
+    kit = IMGKit.new("http://#{site_url}/foodougbar")
+    img = kit.to_img(:png)
+    kit.to_file(Rails.root + "tmp/temp_screengrabs/testimage.png")
   end
 end
